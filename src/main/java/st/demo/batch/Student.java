3ccef979ccb874f799c7b8d6c3d601ef2f508ccc
tengsun <tengsun@cn.ibm.com>
2018-09-05 15:22:18 +0800

    add batch test model and data

---
+++ src/main/java/st/demo/batch/Student.java
@@ -0,0 +1,40 @@
+package st.demo.batch;
+
+import javax.validation.constraints.Size;
+
+public class Student {
+
+	@Size(max=4,min=2)
+	private String name;
+	private Integer age;
+	private String address;
+
+	public Student() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
+/* for security testing */
+
 insert into sys_user (id, username, password) values (1, 'Jack', 'passw0rd');
 insert into sys_user (id, username, password) values (2, 'Rose', 'passw0rd');
 
@@ -6,3 +8,8 @@ insert into sys_role (id, name) values (2, 'ROLE_USER');
 
 insert into sys_user_roles (sys_user_id, roles_id) values (1, 1);
 insert into sys_user_roles (sys_user_id, roles_id) values (2, 2);
+
+/* for batch testing */
+
+drop table if exists student;
+create table student (id int not null primary key, name varchar(20), age int, address varchar(50));
