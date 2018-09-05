e03249c6691fa0a9383d64e830bc1049dfc5843b
tengsun <tengsun@cn.ibm.com>
2018-09-05 14:35:33 +0800

    enable web mvc and security config

--- src/main/java/st/demo/security/WebMvcConfig.java
+++ src/main/java/st/demo/security/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package st.demo.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
